cbd81fbbc06c9d952d6e8038675e1d194a92f7a6
parthergk <gauravkumar81464@gmail.com>
2026-03-01 19:17:54 +0530

    added the api call in assingnment page and error display in both

--- frontend/src/components/Assignment.tsx
+++ frontend/src/components/Assignment.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
 
 const Assignment = () => {
-  const param = useParams()
-  console.log("param", param.id);
-  
+  const { id } = useParams()
+  const [assignment, setAssignment] = useState();
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchAssignment = async () => {
+      setError("");
+      try {
+        const res = await fetch(`http://localhost:5000/api/assignment/${id}`, {
+          method: "GET",
+          credentials: "include"
+        });
+        const data = await res.json();
+        setAssignment(data.data);
+      } catch (error) {
+        console.log("Error fetching assignments", error);
+        setError("Something went wrong. Please try again.");
+      }
+    };
+
+    fetchAssignment();
+  }, [id]);
+
   return (
     <div>Assignment</div>
   )
--- frontend/src/components/Assignments.tsx
+++ frontend/src/components/Assignments.tsx
@@ -10,19 +10,23 @@ type Assignment = {
 };
 
 const Assignments = () => {
+    const [error, setError] = useState("");
     const [assignments, setAssignments] = useState<Assignment[]>([]);
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchAssignments = async () => {
+            setError("");
             try {
                 const res = await fetch("http://localhost:5000/api/assignments", {
+                    method: "GET",
                     credentials: "include"
                 });
                 const data = await res.json();
                 setAssignments(data.data);
             } catch (error) {
                 console.log("Error fetching assignments", error);
+                setError("Something went wrong. Please try again.");
             }
         };
 
@@ -32,8 +36,9 @@ const Assignments = () => {
     return (
         <div className="assignments">
             <h2>Assignments</h2>
+            {error && <p className="assignment__error">{error}</p>}
             <div className="assignments__list">
-                {assignments.map((a) => (
+                {assignments.length > 0 && assignments.map((a) => (
                     <div key={a._id} className="assignments__card">
                         <div>
                             <h3>Q. {a.title}</h3>
@@ -41,7 +46,7 @@ const Assignments = () => {
                             <span className="assignments__difficulty">{a.difficulty}</span>
                         </div>
                         <div className="assignments__btnbox">
-                            <button onClick={()=> navigate(`/assignment/${a._id}`)}>Attempt</button>
+                            <button onClick={() => navigate(`/assignment/${a._id}`)}>Attempt</button>
                         </div>
                     </div>
                 ))}
